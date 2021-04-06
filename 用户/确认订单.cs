3f6de11e5a40ab476e697981d84f3b15a53a5fab
周晨阳 <1448517833@qq.com>
2021-04-06 16:58:41 +0800

    Create 确认订单.cs

---
+++ 用户/确认订单.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 用户
+{
+    public partial class 确认订单 : Form
+    {
+        public 确认订单()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            支付 z = new 支付();
+            z.Show();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            收货地址 s = new 收货地址();
+            s.Show();
+        }
+    }
+}
