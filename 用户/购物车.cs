5781bde3226739d63229931dbed907036f1bae69
周晨阳 <1448517833@qq.com>
2021-04-06 16:58:54 +0800

    Create 购物车.cs

---
+++ 用户/购物车.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 用户
+{
+    public partial class 购物车 : Form
+    {
+        public 购物车()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            确认订单 q = new 确认订单();
+            q.Show();
+        }
+    }
+}
