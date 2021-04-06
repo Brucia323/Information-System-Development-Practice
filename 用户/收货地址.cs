fa4014a8bd8a8387ea99e02add16eca525db3d36
周晨阳 <1448517833@qq.com>
2021-04-06 16:58:09 +0800

    Create 收货地址.cs

---
+++ 用户/收货地址.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 用户
+{
+    public partial class 收货地址 : Form
+    {
+        public 收货地址()
+        {
+            InitializeComponent();
+        }
+    }
+}
