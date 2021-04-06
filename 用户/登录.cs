1f6b31e5669a74b1015f3a53463d3f5838cfd874
周晨阳 <1448517833@qq.com>
2021-04-06 16:58:35 +0800

    Create 登录.cs

---
+++ 用户/登录.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 用户
+{
+    public partial class 登录 : Form
+    {
+        public 登录()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            忘记密码 w = new 忘记密码();
+            w.ShowDialog();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            注册 z = new 注册();
+            z.ShowDialog();
+        }
+
+        private void 登录_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
